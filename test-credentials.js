1f5794465e76418c9e1e455de70051a9a674377b
Linuxdict <edy@linuxdict.com>
2023-01-17 09:17:55 +0800

    Update test-credentials.js

--- test-credentials.js
+++ test-credentials.js
@@ -1,1 +1,3 @@
 const apikey = 'H6XyiCT0w1t9GgTjqhRXxDMrVj9h78ya3NuxlwM7XUs';
+const aws_access_key_id = 'AKIA2USZZXJH3HHN31X4'
+const aws_secret_access_key = '1AkC9YC/1fDIw/v3eXn1v94OAYfrVGE7O7ZMYT9a'
